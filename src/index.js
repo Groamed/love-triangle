0f9c4ee338605ac39ebdd403a9646381867f2ec5
Павел Тамилин <you@example.com>
2018-09-16 02:13:14 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -4,4 +4,22 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var unic = 0;
+  var reff = 0,refs = 0,reft = 0;
+  for(var i = 0;i<preferences.length;i++)
+  {
+    reff = preferences[i] - 1;
+    refs = preferences[reff] - 1;
+    reft = preferences[refs] - 1;
+    if(reff==refs)
+      continue;
+    if(i==reft)
+    {
+      preferences[refs] = 0;
+      preferences[reff] = 0;
+      preferences[i] = 0;
+      unic++;
+    }
+  }
+  return unic;
 };
